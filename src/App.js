36f57415db31a4842beec8045528728a58c5ba0c
charmi <charmi@zuru.tech>
2019-04-04 10:36:45 +0530

    feat: calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Keys from "./components/keys";
+import Answer from "./components/answer";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+        result: ""
+    }
+}
+
+onClick = button => {
+
+    if(button === "="){
+        this.calculate()
+    }
+
+    else if(button === "C"){
+        this.reset()
+    }
+    else if(button === "CE"){
+        this.backspace()
+    }
+
+    else {
+        this.setState({
+            result: this.state.result + button
+        })
+    }
+};
+
+
+calculate = () => {
+    var checkResult = ''
+    if(this.state.result.includes('--')){
+        checkResult = this.state.result.replace('--','+')
+    }
+
+    else {
+        checkResult = this.state.result
+    }
+
+    try {
+        this.setState({
+            result: (eval(checkResult) || "" ) + ""
+        })
+    } catch (e) {
+        this.setState({
+            result: "error"
+        })
+
+    }
+};
+
+reset = () => {
+    this.setState({
+        result: ""
+    })
+};
+
+backspace = () => {
+    this.setState({
+        result: this.state.result.slice(0, -1)
+    })
+};
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <div className="calculator-body">
+                <h1>Simple Calculator</h1>
+                <Answer result={this.state.result} />
+                <Keys onClick={this.onClick} />
+            </div>
+        </div>
     );
   }
 }
